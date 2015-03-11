4d0f78f0d53b76e4fc5c159bbf47f68a315fb5cd
sogolm <sogol.moshtaghi@gmail.com>
2015-03-11 14:16:17 -0700

    adding search

--- search.py
+++ search.py
@@ -9,17 +9,15 @@ from sklearn.metrics.pairwise import linear_kernel
 
 _kmeans_model = None
 _feature_mat_model = None
-
-#TODO: change this to point to the real location. UNCOMMENT
-_kmeans_file = '.../kmeans.pkl'
-_feature_mat_file = '.../featurematrix.pkl'
+_kmeans_file = 'kmeans.pkl'
+_feature_mat_file = 'featurematrix.pkl'
 
 
 def get_kmeans_model():
     global _kmeans_model
     if _kmeans_model is None:
         with open(_kmeans_file, 'r') as f:
-            _model = joblib.load(_kmeans_file)
+            _kmeans_model = joblib.load(_kmeans_file)
     return _kmeans_model
 
 
@@ -27,7 +25,7 @@ def get_feature_mat_model():
     global _feature_mat_model
     if _feature_mat_model is None:
         with open(_feature_mat_file, 'r') as f:
-            _model = joblib.load(_feature_mat_model)
+            _feature_mat_model = joblib.load(_feature_mat_file)
     return _feature_mat_model
 
 
@@ -40,17 +38,17 @@ def calculate_tfidf(query):
     query_count_vect = count_vect.fit_transform([query]).toarray()
     vocabs = count_vect.vocabulary_
 
-    c.execute('''SELECT COUNT(*) FROM urls_sub;''')
+    c.execute('''SELECT COUNT(*) FROM urls;''')
     doc_count = int(c.fetchone()[0])
 
     tfidf = {}
 
     for word, idx in vocabs.iteritems():
-        c.execute(''' SELECT id FROM words_sub
+        c.execute(''' SELECT id FROM words
                       WHERE word='%s';''' %word)
         word_id = int(c.fetchone()[0])
 
-        c.execute('''SELECT url_count FROM word_url_cnt_sub
+        c.execute('''SELECT url_count FROM word_url_cnt
                      WHERE word_id=%d;'''%word_id)
         url_cnt = int(c.fetchone()[0])
 
@@ -65,10 +63,9 @@ def construct_feature_vector(tfidf_dict):
     Constructs the feature vector representation
     of user input based on its tfidf
     """
-    c.execute('''SELECT COUNT(*) FROM words_sub;''')
+    c.execute('''SELECT COUNT(*) FROM words;''')
     word_count = int(c.fetchone()[0])
 
-    #TODO: maybe convert this to a list?
     query_vect = lil_matrix((1, word_count), dtype=np.float_)
 
     for word_id, tfidf in tfidf_dict.iteritems():
@@ -81,9 +78,8 @@ def classify(q_vect):
     Classifies the user input into a cluster
     using a pre-trained model on the corpus
     """
-    #kmeans_model = get_kmeans_model()
-    #TODO: make sure that using predict is the right thing
-    #cluster = kmeans_model.predict(q_vect)
+    get_kmeans_model()
+    cluster = _kmeans_model.predict(q_vect)
     return cluster
 
 
@@ -93,35 +89,49 @@ def compute_cosine_sim(q_vect, cluster):
     user input and every other article in
     cluster and returns the top results.
     """
-    feature_mat = get_feature_mat_model()
-    c.execute('''SELECT url_id FROM url_label_sub
+    get_feature_mat_model()
+    c.execute('''SELECT url_id FROM url_label
                  WHERE label=%d'''%cluster)
+
+    print "querying the urls from labels"
     urls = c.fetchall()  # returns a list of tuples
     urls = np.array(urls)[:, 0]
-    urls_in_clusters = feature_mat[urls]
+    print "number of urls: %d" %len(urls)
+    print _feature_mat_model.shape
+    #urls_in_clusters = _feature_mat_model[urls] #TODO: commenting out for now. too slow!
+    indices = np.arange(len(urls))
+    np.random.shuffle(indices)
+    urls_in_clusters = _feature_mat_model[indices[:100]]
+    print urls_in_clusters.shape
+    print "got urls in cluster"
     cosine_similarities = linear_kernel(q_vect, urls_in_clusters).flatten()
+    print "got cosine similarity"
     sorted_url_ids = cosine_similarities.argsort()[:-5:-1]  # TODO: for now just get the top 5. fix this later
+    print "sorted urls"
     return sorted_url_ids
 
 
 def get_top_results(sorted_urls_ids):
+    print "getting top results from ranked urls by ids"
     id_str = ', '.join(map(str, sorted_url_ids))
-    c.execute('''SELECT url FROM urls_sub WHERE id IN (%s);'''%id_str)
+    print id_str
+    c.execute('''SELECT url FROM urls WHERE id IN (%s);'''%id_str)
     results = c.fetchall()
     results = np.array(results).flatten()
     return results
 
 
 if __name__ == '__main__':
-    query = 'big data, Machine Learning, Google.'
-    conn = psycopg2.connect(dbname='articles', user='sogolmoshtaghi', host='localhost')
+    query = 'big data machine learning at Google.'
+    conn = psycopg2.connect(dbname='articles', user='postgres', password='', host='localhost')
     c = conn.cursor()
     tfidf_dict = calculate_tfidf(query)
     query_vector = construct_feature_vector(tfidf_dict)
     cluster = classify(query_vector)
-    #sorted_url_ids = compute_cosine_sim(query_vector, cluster)
-    sorted_url_ids = [0, 5, 1002, 573]
+    sorted_url_ids = compute_cosine_sim(query_vector, cluster)
     results = get_top_results(sorted_url_ids)
+    print results
     c.close()
     conn.close()
+
 
